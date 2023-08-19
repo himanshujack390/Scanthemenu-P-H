0a92ae65c7a9351804f4f95943206811e565d72d
Tejas Dhodi <tejasdhodi77@gmail.com>
2023-08-19 22:03:15 +0530

    Shift the Code to Route

--- Scanthemenu/src/components/Admin/AdminLogin/Login.jsx
+++ Scanthemenu/src/components/Admin/AdminLogin/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
        
         
         try {
-            const response = await axios.post('http://localhost:5000/login', {
+            const response = await axios.post('http://localhost:3000/login', {
               username,
               password,
             });
--- Server/server.js
+++ Server/server.js
@@ -1,33 +1,18 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+dotenv.config({path: "./Config/.env"});
 
-// Dummy user data
-const users = [
-  { id: 1, username: 'IamAdmin', password: 'Admin@123' },
-  // Add more users
-];
-
-// Login route
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  const user = users.find((u) => u.username === username && u.password === password);
-
-  if (user) {
-    res.json({ success: true, message: 'Login successful!' });
-  } else {
-    res.json({ success: false, message: 'Invalid credentials.' });
-  }
-});
+app.use(express.json())
+app.use(require("./Routes/AdminAuth"))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
